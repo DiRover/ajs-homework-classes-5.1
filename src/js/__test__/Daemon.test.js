759f25242a3b72d581524f386fef8cc9f774fbce
Dmitriy Rovenskiy <arhitec@bk.ru>
2020-07-07 21:47:56 +0300

    Correct type of Character

--- src/js/__test__/Daemon.test.js
+++ src/js/__test__/Daemon.test.js
@@ -1,7 +1,7 @@
 import Daemon from '../Daemon';
 
 test('create character Daemon', () => {
-    const expected = { name: 'Hellscream', type: 'Magician', health: 100, level: 1, attack: 10, defence: 40 };
-    const received = new Daemon('Hellscream', 'Magician', 100, 1, 10, 40);
+    const expected = { name: 'Hellscream', type: 'Daemon', health: 100, level: 1, attack: 10, defence: 40 };
+    const received = new Daemon('Hellscream', 'Daemon', 100, 1, 10, 40);
     expect(received).toEqual(expected);
   });
--- src/js/__test__/Zombie.test.js
+++ src/js/__test__/Zombie.test.js
@@ -1,7 +1,7 @@
 import Zombie from '../Zombie';
 
 test('create character Zombie', () => {
-    const expected = { name: 'Brainlover', type: 'Magician', health: 100, level: 1, attack: 40, defence: 10 };
-    const received = new Zombie('Brainlover', 'Magician', 100, 1, 40, 10);
+    const expected = { name: 'Brainlover', type: 'Zombie', health: 100, level: 1, attack: 40, defence: 10 };
+    const received = new Zombie('Brainlover', 'Zombie', 100, 1, 40, 10);
     expect(received).toEqual(expected);
   });
